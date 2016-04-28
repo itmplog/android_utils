a17c60624c51c6a7995655c3dc3666654c03f097
huaixiaoz <hello@itmp.top>
2016-04-28 06:07:59 +0000

    apt-get install kbtin

--- utils/build.sh
+++ utils/build.sh
@@ -7,6 +7,7 @@ rm -rf out
 repo sync -j12 2>&1 | tee logs/repo_$DATE.log
 ### generate changelog
 
+### apt-get install kbtin
 /build/android_utils/utils/generate_changelog.sh
 
 source build/envsetup.sh
